56efa4d8a23667e0f603c32472d621fcdc515fd1
Yajushreshtha <97739937+Soyvor@users.noreply.github.com>
2026-05-21 17:00:31 +0530

    Delete extension directory

--- extension/material-entry.js
+++
@@ -1,8 +0,0 @@
-import '@material/web/button/filled-button.js';
-import '@material/web/button/filled-tonal-button.js';
-import '@material/web/button/text-button.js';
-import '@material/web/textfield/outlined-text-field.js';
-import '@material/web/switch/switch.js';
-import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
-
-document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
